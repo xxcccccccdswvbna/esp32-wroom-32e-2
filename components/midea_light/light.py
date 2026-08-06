23ac6ada5e0ab2c6be2b3bb4ffdbe2eded6730ad
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-08-06 11:39:48 +0800

    Update light.py

--- components/midea_light/light.py
+++ components/midea_light/light.py
@@ -1,20 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
-from esphome.const import CONF_ID, CONF_MAC
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['ble_tx']
+
+# 🔥 自定义 mac 配置键
+CONF_DEVICE_MAC = "mac"
 
 midea_light_ns = cg.esphome_ns.namespace('midea_light')
 MideaLight = midea_light_ns.class_('MideaLight', light.LightOutput, cg.Component)
 
 CONFIG_SCHEMA = light.BRIGHTNESS_COLOR_TEMPERATURE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MideaLight),
-    cv.Required(CONF_MAC): cv.string,
+    cv.Required(CONF_DEVICE_MAC): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await light.register_light(var, config)
-    cg.add(var.set_mac(config[CONF_MAC]))
+    cg.add(var.set_mac(config[CONF_DEVICE_MAC]))
