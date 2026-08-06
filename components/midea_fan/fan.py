fc1362296b20436f81b914499619ab1b73e2a9de
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-08-06 11:40:05 +0800

    Update fan.py

--- components/midea_fan/fan.py
+++ components/midea_fan/fan.py
@@ -1,21 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan
-from esphome.const import CONF_ID, CONF_MAC
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['ble_tx']
+
+CONF_DEVICE_MAC = "mac"
 
 midea_fan_ns = cg.esphome_ns.namespace('midea_fan')
 MideaFan = midea_fan_ns.class_('MideaFan', fan.Fan, cg.Component)
 
 CONFIG_SCHEMA = fan.FAN_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MideaFan),
-    cv.Required(CONF_MAC): cv.string,
+    cv.Required(CONF_DEVICE_MAC): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await fan.register_fan(var, config)
-    cg.add(var.set_mac(config[CONF_MAC]))
+    cg.add(var.set_mac(config[CONF_DEVICE_MAC]))
     cg.add(var.set_speed_count(6))
