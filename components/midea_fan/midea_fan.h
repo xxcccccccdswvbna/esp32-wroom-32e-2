6c422403ceec3c50d974ef52ed750361e2ffcaf0
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-08-06 10:53:15 +0800

    Create midea_fan.h

---
+++ components/midea_fan/midea_fan.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/fan/fan.h"
+
+namespace esphome {
+namespace midea_fan {
+
+class MideaFan : public fan::Fan, public Component {
+public:
+    void set_mac(const std::string &mac) { mac_ = mac; }
+    
+    fan::FanTraits get_traits() override {
+        return fan::FanTraits(false, true, false, 6);
+    }
+    
+    void write_state() override {
+        std::string cmd;
+        if (state) {
+            cmd = midea_fan_on(mac_, speed);
+        } else {
+            cmd = midea_fan_off(mac_);
+        }
+        ble_tx_send(cmd);
+    }
+    
+private:
+    std::string mac_;
+};
+
+} // namespace midea_fan
+} // namespace esphome
