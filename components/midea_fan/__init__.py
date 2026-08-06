afa6e77bc5392ca9ae28f6c49e0e78851a80a497
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-08-06 15:13:37 +0800

    Delete components directory

--- components/midea_fan/__init__.py
+++
@@ -1,3 +0,0 @@
-import esphome.codegen as cg
-midea_fan_ns = cg.esphome_ns.namespace('midea_fan')
-MideaFan = midea_fan_ns.class_('MideaFan', cg.Component)
--- components/midea_light/__init__.py
+++
@@ -1,3 +0,0 @@
-import esphome.codegen as cg
-midea_light_ns = cg.esphome_ns.namespace('midea_light')
-MideaLight = midea_light_ns.class_('MideaLight', cg.Component)
